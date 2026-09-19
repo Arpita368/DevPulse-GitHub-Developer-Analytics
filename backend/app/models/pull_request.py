e77319d723abfdb96f7c652d6941599d975fcb3d
Arpita Sonparote <arpita.j.s.368@gmail.com>
2026-09-19 16:11:27 +0530

    feat: add pull-request database model

---
+++ backend/app/models/pull_request.py
@@ -0,0 +1,67 @@
+import uuid
+from datetime import datetime
+
+from sqlalchemy import BigInteger, DateTime, ForeignKey, Integer, Text
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.db.base import Base
+
+class PullRequest(Base):
+    __tablename__ = "pull_requests"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        primary_key=True,
+        default=uuid.uuid4
+    )
+
+    repository_id: Mapped[uuid.UUID] = mapped_column(
+        ForeignKey("repositories.id", ondelete="CASCADE"),
+        nullable=False
+    )
+
+    github_id: Mapped[int] = mapped_column(
+        BigInteger,
+        nullable=False,
+        unique=True
+    )
+
+    number: Mapped[int] = mapped_column(
+        Integer,
+        nullable=False
+    )
+
+    title: Mapped[str] = mapped_column(
+        Text,
+        nullable=False
+    )
+
+    state: Mapped[str] = mapped_column(
+        Text,
+        nullable=False
+    )
+
+    author_username: Mapped[str] = mapped_column(
+        Text
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False
+    )
+
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False
+    )
+
+    closed_at: Mapped[datetime | None] = mapped_column(
+        DateTime(timezone=True),
+    )
+
+    merged_at: Mapped[datetime | None] = mapped_column(
+        DateTime(timezone=True),
+    )
+
+    url: Mapped[str | None] = mapped_column(
+        Text
+    )
